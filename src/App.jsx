b2eaf600e09cd7a417c4d16c1b0c18a4d2362413
Sandra-18 <sanlorenita-18@hotmail.com>
2023-03-11 13:42:36 -0500

    se crea repositorio de BurguerQueen

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <nav className="navegador">
+        <a href="">Clientes</a>
+        <a href="">Productos</a>
+        <a href="">Sedes</a>
+      </nav>
+    </main>
+  );
+};
+export default App;
